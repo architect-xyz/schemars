38d76563b939b6ae6bc930137a1fd68839f16c97
Dustan Kasten <dustan@architect.co>
2025-01-02 10:09:07 -0500

    Naive chrono_tz::Tz impl

--- schemars/src/json_schema_impls/chrono.rs
+++ schemars/src/json_schema_impls/chrono.rs
@@ -57,3 +57,19 @@ formatted_string_impl!(NaiveDate, "date");
 formatted_string_impl!(NaiveDateTime, "partial-date-time");
 formatted_string_impl!(NaiveTime, "partial-date-time");
 formatted_string_impl!(DateTime, "date-time", <Tz: TimeZone> JsonSchema for DateTime<Tz>);
+
+impl JsonSchema for chrono_tz::Tz {
+    no_ref_schema!();
+
+    fn schema_name() -> String {
+        "TimeZone".to_owned()
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            ..Default::default()
+        }
+        .into()
+    }
+}
